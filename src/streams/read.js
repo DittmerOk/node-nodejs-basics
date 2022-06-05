e3fbc667dd6b65e0811aad55449c02b4b73fe5ee
Andrey <gripicha@yandex.by>
2022-06-05 19:38:55 +0300

    add solution streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,18 @@
+import path, { dirname } from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PATH = path.join(__dirname, "./files", "fileToRead.txt");
+
 export const read = async () => {
-    // Write your code here 
+  const stream = fs.createReadStream(PATH);
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
 };
+
+await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,15 @@
+import { Transform } from "stream";
+
 export const transform = async () => {
-    // Write your code here 
+  process.stdout.write("Type text and press Enter to reverse it: \n");
+
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split("").reverse().join("") + "\n\n");
+    },
+  });
+
+  process.stdin.pipe(reverse).pipe(process.stdout);
 };
+
+await transform();
